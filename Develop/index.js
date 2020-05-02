3d471ae14258e0d344231b6aa3630d424de20de9
Drew Swinicki <dswinicki93@gmail.com>
2020-05-02 10:14:45 -0400

    updated axios call url and is functional

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require('fs');
 
@@ -59,9 +60,25 @@ inquirer.prompt([
 
   .then(function (answers) {
     console.log(answers);
- 
 
-let document = `
+
+    let userInfo = answers.github;
+    // console.log(userInfo);
+
+    function searchUser() {
+
+      console.log(userInfo);
+
+      axios
+        .get("https://api.github.com/users/" + userInfo)
+        .then(function (res) {
+          console.log(res.data);
+        });
+    };
+    searchUser();
+
+
+    let document = `
   ## Title:
   ${answers.project}
   ## Description of Application:
@@ -81,22 +98,23 @@ let document = `
   ## Tests:
   ${answers.tests}
   ## Github
-  ${answers.github}
+ 
 
   `;
+    searchInfo();
 
-fs.writeFile("READMEnew.md", document, function (err, data) {
-  if (err) {
-    console.log(err);
-  }
-})
-});
 
-function writeToFile(fileName, data) {
-}
+    fs.writeFile("READMEnew.md", document, function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  });
+
 
 function init() {
 
 }
 
 init();
+
