63aa75b7a814b300c8ba3c02c2007dcc1b76ec67
Drew Swinicki <dswinicki93@gmail.com>
2020-05-06 13:28:58 -0400

    added created readme and updated index

---
+++ Develop/createdreadme.md
@@ -0,0 +1,44 @@
+# readme generator[![Website monip.org](https://img.shields.io/website-up-down-green-red/http/monip.org.svg)](http://monip.org/)
+
+ 
+  ## Table of Contents
+  
+  - [Description](#description)
+  - [Installing and Updating](#installing-and-updating)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Tests](#running-tests)
+  - [Contributing](#contributing)
+  - [GitHub Details](#github-details)
+      - [GitHub profile picture](#github-profile-picture)
+      - [GitHub email](#github-email)
+  
+  ## Description
+  
+  write app that creates a readme based on user input
+  
+  ## Installation and Updating
+  
+  
+  
+  ## Usage
+  
+  
+  
+  ## Licenses
+  
+  
+  
+  ## Tests
+  
+  
+  
+  ## Contributes
+  
+  
+  
+  ## GitHub Information 
+  
+  GitHub Avatar: https://avatars2.githubusercontent.com/u/57301548?v=4
+  
+  GitHub email: null
--- Develop/index.js
+++ Develop/index.js
@@ -1,124 +1,123 @@
-const axios = require("axios");
 const inquirer = require("inquirer");
-const fs = require('fs');
-let githubPicture;
-let githubEmail;
+const fs = require("fs");
+const axios = require("axios");
+const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
-inquirer.prompt([
-  {
-    message: "What's your project name?",
-    name: "project",
-    type: "input"
-  },
-  {
-    message: "Tell me about your project",
-    name: "description",
-    type: "input"
-  },
-  {
-    message: "Table of Contents",
-    name: "contents",
-    type: "input"
-  },
-  {
-    message: "Installation",
-    name: "installation",
-    type: "input"
-  },
-  {
-    message: "What is this apps intended usage?",
-    name: "usage",
-    type: "input"
-  },
-  {
-    message: "License",
-    name: "License",
-    type: "input"
-  },
-  {
-    message: "Badges",
-    name: "badges",
-    type: "input"
-  },
-  {
-    message: "Contributors",
-    name: "contributors",
-    type: "input"
-  },
-  {
-    message: "Tests",
-    name: "tests",
-    type: "input"
-  },
-  {
-    message: "Whats your github url?",
-    name: "github",
-    type: "input"
-  }
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Please enter a description of your project.",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Please detail the installation guidelines.",
+      name: "install"
+    },
+    {
+      type: "input",
+      message: "Please detail the intended usage of your project.",
+      name: "usages"
+    },
+    {
+      type: "input",
+      message: "Please indicate the license details.",
+      name: "licenses"
+    },
+    {
+      type: "input",
+      message: "Please note who contributed to the project.",
+      name: "contributes"
+    },
+    {
+      type: "input",
+      message: "Please describe what kind of tests were performed.",
+      name: "testing"
+    }
+  ])
+}; 
 
-])
+async function githubInfo(username){
+  const queryUrl = `https://api.github.com/users/${username}?client_id=22779517f8aa3cdaf282&
+  client_secret=387369c36a454088eceea49ebb5101734049ddd3`;
+  
+  const data = await axios
+    .get(queryUrl)
+    .then(function(response) { return response.data })
+  return data;
+};
 
-  .then(function (answers) {
-    console.log(answers);
+function generateFile(answers, email, avatar_url) {
+  return `# ${answers.title}[![Website monip.org](https://img.shields.io/website-up-down-green-red/http/monip.org.svg)](http://monip.org/)
 
-
-    let userInfo = answers.github;
-    // console.log(userInfo);
-
-    function searchUser() {
-
-      console.log(userInfo);
-
-      axios
-        .get("https://api.github.com/users/" + userInfo)
-        .then(function (res) {
-          console.log(res.data);
-          githubEmail = res.data.email;
-          githubPicture = res.data.avatar_url;
-        });
-    };
-    searchUser();
-
-
-    let document = `
-  ## Title:
-  ${answers.project}
-  ## Description of Application:
-  ${answers.description}
-  ## Table of Contents:
-  ${answers.contents}
-  ## Installation:
-  ${answers.installation}
-  ## Usage:
-  ${answers.usage}
-  ## License:
-  ${answers.license}
-  ## Badges:
-  ${answers.badges}
-  ## Contributing:
-  ${answers.contributors}
-  ## Tests:
-  ${answers.tests}
-  ## Github
-  ${githubPicture}
-  ${githubEmail}
  
+  ## Table of Contents
+  
+  - [Description](#description)
+  - [Installing and Updating](#installing-and-updating)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Tests](#running-tests)
+  - [Contributing](#contributing)
+  - [GitHub Details](#github-details)
+      - [GitHub profile picture](#github-profile-picture)
+      - [GitHub email](#github-email)
+  
+  ## Description
+  
+  ${answers.description}
+  
+  ## Installation and Updating
+  
+  ${answers.install}
+  
+  ## Usage
+  
+  ${answers.usages}
+  
+  ## Licenses
+  
+  ${answers.licenses}
+  
+  ## Tests
+  
+  ${answers.testing}
+  
+  ## Contributes
+  
+  ${answers.contributes}
+  
+  ## GitHub Information 
+  
+  GitHub Avatar: ${avatar_url}
+  
+  GitHub email: ${email}`
+}; 
 
-  `;
-    
+async function init() {
+  try {
+    const answers = await promptUser();
+    const {email, avatar_url} = await githubInfo(answers.username)
+    const file = generateFile(answers, email, avatar_url); 
+    await writeFileAsync("readme.md", file); 
 
-
-    fs.writeFile("READMEnew.md", document, function (err, data) {
-      if (err) {
-        console.log(err);
-      }
-    })
-  });
-
-
-function init() {
-
+    console.log("Successfully generated README.md file!");
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 init();
